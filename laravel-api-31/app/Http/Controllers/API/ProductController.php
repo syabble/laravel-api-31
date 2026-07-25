1c4385e72422190f405a0c210e6e6981bbab7759
syafiraa <safirawijayanti587@gmail.com>
2026-07-25 12:21:25 +0700

    project_migrasi_model

---
+++ laravel-api-31/app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(10);
+        return response()->json(
+            new ProductCollection($products),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
